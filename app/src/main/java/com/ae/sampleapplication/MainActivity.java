9410ec3e4696192990f544d45fadded17beeb620
Srianju <anjuchowdary93@gmail.com>
2021-12-29 08:31:01 +0530

    first commit

---
+++ app/src/main/java/com/ae/sampleapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ae.sampleapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Life Cycle Methods -> Paused, Stopped -> BAckground, Resumed -> Foreground
+}
